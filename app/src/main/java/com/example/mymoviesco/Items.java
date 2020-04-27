1ff62010cd61fb69ec3da1bc6ee761147516668b
SilverDove <tricot.clara99@gmail.com>
2020-04-27 11:24:05 +0200

    Initial commit

---
+++ app/src/main/java/com/example/mymoviesco/Items.java
@@ -0,0 +1,25 @@
+package com.example.mymoviesco;
+
+public class Items {
+    private int mImage;
+    private String mTitle;
+    private String mDescription;
+
+    public Items(int image, String title, String description){
+        mImage=image;
+        mTitle=title;
+        mDescription=description;
+    }
+
+    public int getImage(){
+        return mImage;
+    }
+
+    public String getTitle(){
+        return mTitle;
+    }
+
+    public String getDescription(){
+        return mDescription;
+    }
+}
